0a00a90a0d6333c5d0ecfb34eeba4194964ab4c0
sankalpa <sankalpaacharya6609@gmail.com>
2022-02-25 23:35:55 +0545

    made base template generator

--- helper.py
+++ helper.py
@@ -1,6 +1,16 @@
 import os
 import subprocess
 
+
+
+
+def base_temp():
+    os.mkdir('templates')
+    with open('templates/base.html','w') as f:
+        pass
+
+
+
 def changePath(path):
     if path is not None:
         try:
@@ -14,4 +24,9 @@ def _project(name):
 
 def _app(name,project):
     changePath(project)
+    base_temp()
     subprocess.run([f'python','manage.py','startapp',name],shell=True)
+
+
+
+
